16177af31ae6f0228be22aee27704296ed22259d
Thechosenpj <846550835@qq.com>
2021-12-30 23:39:19 +0800

    vuex

---
+++ vuedemo/src/views/count.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>当前求和为：{{ sum }}</h1>
+    <h3>当前求和放大10倍为：{{ pCount }}</h3>
+    <p>我在{{imss}}，练习{{proge}}</p>
+    <el-select v-model="num">
+      <el-option
+      v-for="(item,index) in list"
+      :key="index"
+      :label="item.label"
+      :value="item.value"
+    >
+      </el-option>
+    </el-select>
+    <el-button @click="add">+</el-button>
+    <el-button @click="odd">-</el-button>
+    <el-button>为奇数时在+</el-button>
+    <el-button>等一等在+</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      list: [
+        { label: 1, value: 1 },
+        { label: 2, value: 2 },
+        { label: 3, value: 3 }
+      ],
+      num: ''
+    }
+  },
+  computed: {
+    sum () {
+      return this.$store.state.sum
+    },
+    pCount () {
+      return this.$store.getters.pCount
+    },
+    imss () {
+      return this.$store.state.imss
+    },
+    proge () {
+      return this.$store.state.proge
+    }
+  },
+  methods: {
+    add () {
+      this.$store.commit('jia', this.num)
+    },
+    odd () {
+      this.$store.commit('jian', this.num)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuedemo/src/views/datePicker.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="datePicker">
+    <el-date-picker
+      v-model="value"
+      type="datetimerange"
+      :default-value="data.time"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期"
+    ></el-date-picker>
+    <el-button @click="clear">重 置</el-button>
+    <el-date-picker
+      v-model="value"
+      type="datetime"
+      :default-value="data.time"
+      placeholder="开始日期"
+    ></el-date-picker>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: '',
+      startTime: '',
+      endTime: '',
+      data: { time: new Date() },
+      data2: { time2: new Date() },
+      arr: []
+    }
+  },
+  created () {
+    // const date = new Date()
+    // this.startTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    // this.endTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    // console.log('date', this.startTime)
+    // this.arr.push(this.startTime)
+    // this.arr.push(this.endTime)/*  */
+    // this.data = new Date()
+  },
+  methods: {
+    clear () {
+      this.value = ''
+      const date = new Date()
+      this.startTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+      this.endTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+      this.arr = []
+      this.arr.push(this.startTime)
+      this.arr.push(this.endTime)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
